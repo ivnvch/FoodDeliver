5a0e7e00b431834454ce06761ccdf57b05c8d0f9
Dmitry <tarasovets.dmtiv@gmail.com>
2023-06-11 11:16:46 +0300

    feat: add Response class

---
+++ FoodDelivery.Models/Repsonse/BaseResponse.cs
@@ -0,0 +1,18 @@
+﻿using FoodDelivery.Models.Enum;
+
+namespace FoodDelivery.Models.Repsonse
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public T Data { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public string Description { get; set; }
+    }
+
+    public interface IBaseResponse<T>
+    {
+        T Data { get; set; }
+        StatusCode StatusCode { get; set; }
+        string Description { get; set; }
+    }
+}
