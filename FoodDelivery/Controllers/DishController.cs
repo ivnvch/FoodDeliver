6b5b856860fc4b54a3e5e4143357a6205a79d183
Gyvadio <nilf.gorn@mail.ru>
2023-06-18 14:02:19 +0300

    fix

---
+++ FoodDelivery/Controllers/DishController.cs
@@ -0,0 +1,65 @@
+﻿using Azure;
+using FoodDelivery.Models.Entity.DTOs;
+using FoodDelivery.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDelivery.Controllers
+{
+    public class DishController : Controller
+    {
+       
+        private readonly IDishService _dishService;
+
+
+        public DishController(IDishService dishService)
+        {
+            this._dishService = dishService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDishes()
+        {
+            var response = await _dishService.GetAll();
+            if (response.StatusCode == Models.Enum.StatusCode.OK)
+            {
+                return Ok(response);
+            }
+            return BadRequest();
+        }
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateDishes(DishUpdateDTO dish)
+        {
+            if (ModelState.IsValid)
+            {
+                var responce = await _dishService.Update(dish);
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return View(dish);
+            }
+        }
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateDishes(DishCreateDTO entity)
+        {
+            if (ModelState.IsValid)
+            {
+               var dish = await _dishService.Create(entity);
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return View(entity);
+            }
+
+        }
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteDish(int id)
+        {
+
+            var responce = await _dishService.Delete(id);
+            return RedirectToAction("List");
+
+        }
+    }
+}
