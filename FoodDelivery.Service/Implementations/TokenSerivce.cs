4f16c03044284a3d75e764d65aa9ce8db1a637ef
Dmitry <tarasovets.dmtiv@gmail.com>
2023-06-19 15:28:29 +0300

    feat: add TokenService

---
+++ FoodDelivery.Service/Implementations/TokenSerivce.cs
@@ -0,0 +1,46 @@
+﻿using FoodDelivery.Models.Entity;
+using FoodDelivery.Service.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FoodDelivery.Service.Implementations
+{
+    public class TokenSerivce : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenSerivce(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:TokenKey"]));
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,user.Login)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(int.Parse(_configuration["Jwt:TokenLifetimeMinutes"])),
+                SigningCredentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature),
+            };
+
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ FoodDelivery.Service/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using FoodDelivery.Models.Entity;
+using FoodDelivery.Models.ViewModel.User;
+
+namespace FoodDelivery.Service.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(User user);
+    }
+}
