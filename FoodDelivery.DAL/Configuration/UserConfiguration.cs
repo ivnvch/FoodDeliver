b186573974c2d0715cbb74e90dda337667e16b22
Dmitry <tarasovets.dmtiv@gmail.com>
2023-06-21 21:38:22 +0300

    refactor: add Configuration Class in DAL

---
+++ FoodDelivery.DAL/Configuration/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using FoodDelivery.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace FoodDelivery.DAL.Configuration
+{
+    internal class UserConfiguration: IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users").HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Login).IsRequired();
+            builder.Property(x => x.PasswordHash).IsRequired();
+            builder.Property(x => x.PasswordSalt).IsRequired();
+
+
+            builder.HasOne(u => u.Profile)
+            .WithOne(p => p.User)
+            .HasForeignKey<Profile>(x => x.UserId).IsRequired();
+
+            builder.HasOne(u => u.Basket)
+            .WithOne(b => b.User)
+            .HasForeignKey<Basket>(x => x.UserId).IsRequired();
+        }
+    }
+}
