fa784c5ba7237fecf459af517258c23e58da6895
Your Name <you@example.com>
2023-06-22 17:59:01 +0300

    add Commentary model

---
+++ FoodDelivery.DAL/Entity/Commentary.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodDelivery.DAL.Entity
+{
+    public class Commentary
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        [ForeignKey("VendorId")]
+        public int VendorId { get; set; }
+        public Vendor? Vendor { get; set; }
+        public double CustomerRating { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
--- FoodDelivery.DAL/Entity/Vendor.cs
+++ FoodDelivery.DAL/Entity/Vendor.cs
@@ -10,6 +10,6 @@ namespace FoodDelivery.DAL.Entity
         public string Type { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public List<Dish> Dishes { get; set; } = new List<Dish> { };
-        public List<double> AllCustomerRatings { get; set; } = new List<double> { };
+        public List<Commentary> Comments { get; set; } = new List<Commentary> { };
     }
 }
