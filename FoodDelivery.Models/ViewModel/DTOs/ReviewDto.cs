f3b5ef8909fb3de485379db4288f57809e54ed6a
Your Name <you@example.com>
2023-06-22 21:16:09 +0300

    add ReviewDto

---
+++ FoodDelivery.Models/ViewModel/DTOs/ReviewDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodDelivery.Models.ViewModel.DTOs
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int UserId { get; set; }
+        public double CustomerRating { get; set; }
+        public string? Description { get; set; } = string.Empty;
+    }
+}
