fbb91a8944410183c811bcd265459f8e587ab4a7
Gyvadio <nilf.gorn@mail.ru>
2023-06-23 12:56:22 +0300

    fix view models

--- FoodDelivery.Models/ViewModel/DishViewModel/DishCreateDTO.cs
+++ FoodDelivery.Models/ViewModel/DishViewModel/DishCreateDTO.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using FoodDelivery.Models.Enum;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoodDelivery.Models.ViewModel.DishViewModel
+namespace FoodDelivery.Models.Entity.DTO
 {
-    internal class Class1
+    public class DishCreateDTO
     {
+
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public double Weight { get; set; }
+        public short Amount { get; set; }
+        public Category Category { get; set; }
     }
 }
--- FoodDelivery.Models/ViewModel/DishViewModel/DishUpdateDTO.cs
+++ FoodDelivery.Models/ViewModel/DishViewModel/DishUpdateDTO.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using FoodDelivery.Models.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoodDelivery.Models.ViewModel.DishViewModel
+namespace FoodDelivery.Models.Entity.DTO
 {
-    internal class Class1
+    public class DishUpdateDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public double Weight { get; set; }
+        public Category Category { get; set; }
     }
 }
