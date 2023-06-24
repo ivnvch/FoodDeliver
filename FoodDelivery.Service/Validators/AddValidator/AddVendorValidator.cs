6935dd3f9d6ee10a02f7bb5a238d904d5cd63797
Your Name <you@example.com>
2023-06-24 13:00:47 +0300

    add UpdateVendorValidator

--- FoodDelivery.Service/Validators/AddValidator/AddVendorValidator.cs
+++ FoodDelivery.Service/Validators/AddValidator/AddVendorValidator.cs
@@ -9,42 +9,47 @@ namespace FoodDelivery.Service.Validators.AddValidator
         {
             RuleFor(v => v.OpeningTime)
                 .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
                 .NotEmpty().WithMessage("data is empty")
                 .Must(IsValidTime).WithMessage("invalid date");
             RuleFor(v => v.ClosingTime)
                .Cascade(CascadeMode.StopOnFirstFailure)
-               .NotEmpty().WithMessage("data is empty")               
+               .NotNull()
+               .NotEmpty().WithMessage("data is empty")
                .Must(IsValidTime).WithMessage("invalid date");
             RuleFor(v => v.TimeOfDelivery)
                .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotNull()
                .NotEmpty().WithMessage("data is empty")
                .Must(IsValidTime).WithMessage("invalid date");
             RuleFor(v => v.Type)
                 .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
                 .NotEmpty().WithMessage("type is empty")
                 .Length(3, 15).WithMessage("length of type invalid")
                 .Must(IsValidName).WithMessage("type contains invalid characters");
             RuleFor(v => v.Name)
                 .Cascade(CascadeMode.StopOnFirstFailure)
-                .NotEmpty().WithMessage("name is empty")
-                .NotNull().WithMessage("name is null")
+                .NotNull()
+                .NotEmpty()
+                .NotNull().WithMessage("name is empty")
                 .Length(2, 20).WithMessage("length of name invalid");
             RuleFor(v => v.Description)
                 .MaximumLength(200).WithMessage("maximum description length must be up to 200");
             RuleFor(v => v.Address)
                .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotNull()
                .NotEmpty().WithMessage("address is empty")
                .Length(10, 100).WithMessage("length of name invalid");
             RuleFor(v => v.PhoneNumber)
                 .Cascade(CascadeMode.StopOnFirstFailure)
-                .NotNull().WithMessage("phone number is null")
+                .NotNull()
                 .NotEmpty().WithMessage("phone number is empty")
                 .MaximumLength(13).WithMessage("phone number length should not be more than 13");
         }
         public bool IsValidName(string name)
         {
             name = name.Replace(" ", "");
-            //name = name.Replace("_", "");
             return name.All(char.IsLetter);
         }
         protected bool IsValidTime(DateTime date)
--- FoodDelivery.Service/Validators/UpdateValidator/UpdateVendorValidator.cs
+++ FoodDelivery.Service/Validators/UpdateValidator/UpdateVendorValidator.cs
@@ -7,18 +7,61 @@ namespace FoodDelivery.Service.Validators.UpdateValidator
     {
         public UpdateVendorValidator()
         {
+            RuleFor(v => v.Id)
+                .NotNull()
+                .NotEmpty().WithMessage("id is requered");
+            RuleFor(v => v.OpeningTime)
+               .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotNull()
+               .NotEmpty().WithMessage("data is empty")
+               .Must(IsValidTime).WithMessage("invalid date");
+            RuleFor(v => v.ClosingTime)
+               .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotNull()
+               .NotEmpty().WithMessage("data is empty")
+               .Must(IsValidTime).WithMessage("invalid date");
+            RuleFor(v => v.TimeOfDelivery)
+               .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotNull()
+               .NotEmpty().WithMessage("data is empty")
+               .Must(IsValidTime).WithMessage("invalid date");
+            RuleFor(v => v.Type)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
+                .NotEmpty().WithMessage("type is empty")
+                .Length(3, 15).WithMessage("length of type invalid")
+                .Must(IsValidName).WithMessage("type contains invalid characters");
             RuleFor(v => v.Name)
                 .Cascade(CascadeMode.StopOnFirstFailure)
-                .NotEmpty().WithMessage("{PropertyName} is empty")
-                .Length(2, 20).WithMessage("length of name invalid")
-                .Must(IsValidName).WithMessage("name contains invalid characters");
-
+                .NotNull()
+                .NotEmpty()
+                .NotNull().WithMessage("name is empty")
+                .Length(2, 20).WithMessage("length of name invalid");
+            RuleFor(v => v.Description)
+                .MaximumLength(200).WithMessage("maximum description length must be up to 200");
+            RuleFor(v => v.Address)
+               .Cascade(CascadeMode.StopOnFirstFailure)
+               .NotNull()
+               .NotEmpty().WithMessage("address is empty")
+               .Length(10, 100).WithMessage("length of name invalid");
+            RuleFor(v => v.PhoneNumber)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
+                .NotEmpty().WithMessage("phone number is empty")
+                .MaximumLength(13).WithMessage("phone number length should not be more than 13");
         }
         public bool IsValidName(string name)
         {
             name = name.Replace(" ", "");
-            name = name.Replace("_", "");
             return name.All(char.IsLetter);
+        }
+        protected bool IsValidTime(DateTime date)
+        {
+            if (date.ToString() != date.ToShortTimeString())
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
