1811f739b938cb36c7ef6380d3084f3846ddcd7f
Your Name <you@example.com>
2023-06-24 14:09:24 +0300

    add UpdateBasketValidator

---
+++ FoodDelivery.Service/Validators/UpdateValidator/UpdateBasketValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FoodDelivery.DAL.Entity;
+
+namespace FoodDelivery.Service.Validators.UpdateValidator
+{
+    public class UpdateBasketValidator:AbstractValidator<Basket>
+    {
+        public UpdateBasketValidator()
+        {
+            RuleFor(b => b.Id)
+                .NotNull()
+                .NotEmpty().WithMessage("id is requered");
+            RuleFor(b => b.UserId)
+                .NotNull()
+                .NotEmpty().WithMessage("user id is requered");
+        }
+    }
+}
